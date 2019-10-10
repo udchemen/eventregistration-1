b43f59852e6d44d7534bc6de84781d7d792e8d7f
Marton Bur <marton.bur@mail.mcgill.ca>
2019-10-10 11:12:42 -0400

    Sharing changes after tutorial 5

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/RegistrationManager.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/RegistrationManager.java
@@ -7,47 +7,48 @@ import javax.persistence.OneToMany;
 import javax.persistence.Id;
 
 @Entity
-public class RegistrationManager{
-private Set<Person> persons;
+public class RegistrationManager {
+	private Set<Person> persons;
 
-@OneToMany(cascade={CascadeType.ALL})
-public Set<Person> getPersons() {
-   return this.persons;
+	@OneToMany(cascade = { CascadeType.ALL })
+	public Set<Person> getPersons() {
+		return this.persons;
+	}
+
+	public void setPersons(Set<Person> personss) {
+		this.persons = personss;
+	}
+
+	private Set<Registration> registrations;
+
+	@OneToMany(cascade = { CascadeType.ALL })
+	public Set<Registration> getRegistrations() {
+		return this.registrations;
+	}
+
+	public void setRegistrations(Set<Registration> registrationss) {
+		this.registrations = registrationss;
+	}
+
+	private Set<Event> events;
+
+	@OneToMany(cascade = { CascadeType.ALL })
+	public Set<Event> getEvents() {
+		return this.events;
+	}
+
+	public void setEvents(Set<Event> eventss) {
+		this.events = eventss;
+	}
+
+	private int id;
+
+	public void setId(int value) {
+		this.id = value;
+	}
+
+	@Id
+	public int getId() {
+		return this.id;
+	}
 }
-
-public void setPersons(Set<Person> personss) {
-   this.persons = personss;
-}
-
-private Set<Registration> registrations;
-
-@OneToMany(cascade={CascadeType.ALL})
-public Set<Registration> getRegistrations() {
-   return this.registrations;
-}
-
-public void setRegistrations(Set<Registration> registrationss) {
-   this.registrations = registrationss;
-}
-
-private Set<Event> events;
-
-@OneToMany(cascade={CascadeType.ALL})
-public Set<Event> getEvents() {
-   return this.events;
-}
-
-public void setEvents(Set<Event> eventss) {
-   this.events = eventss;
-}
-
-private int id;
-
-public void setId(int value) {
-this.id = value;
-    }
-@Id
-public int getId() {
-return this.id;
-       }
-   }
